1a6ce4833b5b992396b1ce68ac0463bcface7c7b
huy01040104 <tronghuy01040@gmail.com>
2024-12-27 11:52:46 +0700

    add: assignees component + share pagination component

--- frontend/src/app/core/layout/header/header.component.ts
+++ frontend/src/app/core/layout/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +9,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements AfterViewInit {
+  constructor(private router: Router, private el: ElementRef, private renderer: Renderer2) {}
+  
+  ngAfterViewInit(): void {
+    if (this.isHomeRoute()) {
+      const mainNavElement = this.el.nativeElement.querySelector('.main-nav');
+  
+      if (mainNavElement) {
+        this.renderer.setStyle(mainNavElement, 'background-color', '#000');
+      }
+    }
+  }
+  
+  isHomeRoute(): boolean {
+    const currentRoute = this.router.url;
+    return currentRoute === '' || currentRoute === '/home';
+  }
 }
