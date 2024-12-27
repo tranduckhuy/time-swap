db55bc96ddd3ed3a39843c066c9fd2ccb1c01ed0
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-12-27 22:18:05 +0700

    add: add entities and constraints definitions

---
+++ backend/TimeSwap.Domain/Entities/JobApplicant.cs
@@ -0,0 +1,14 @@
+﻿namespace TimeSwap.Domain.Entities
+{
+    public class JobApplicant : EntityBase<Guid>
+    {
+        public Guid JobPostId { get; set; }
+
+        public Guid UserAppliedId { get; set; }
+
+        public DateTime AppliedAt { get; set; }
+
+        // Navigation properties
+        public virtual JobPost JobPost { get; set; } = null!;
+    }
+}
---
+++ backend/TimeSwap.Shared/Constants/StatusCode.cs
@@ -0,0 +1,26 @@
+﻿namespace TimeSwap.Shared.Constants
+{
+    /// <summary>
+    /// This class contains all the status codes for the application 1xxx
+    /// </summary>
+    #region SuccessCodes
+    public enum SuccessCode
+    {
+        Register = 1000,
+        Login = 1001,
+        Update = 1002
+    }
+    #endregion
+
+    /// <summary>
+    /// This class contains all the error codes for the application 2xxx
+    /// </summary>
+    #region ErrorCodes
+    public enum ErrorCode
+    {
+        RegisterFailed = 2000,
+        LoginFailed = 2001,
+        ValidationFailed = 2002
+    }
+    #endregion
+}
