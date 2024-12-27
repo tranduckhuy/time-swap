13885f3f1ae2bb948188f31e67b12215a946f124
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-12-27 22:18:05 +0700

    add: add entities and constraints definitions

---
+++ backend/TimeSwap.Domain/Entities/TransactionLog.cs
@@ -0,0 +1,18 @@
+﻿using TimeSwap.Shared.Constants;
+
+namespace TimeSwap.Domain.Entities
+{
+    public class TransactionLog : EntityBase<Guid>
+    {
+        public Guid TransactionId { get; set; }
+        
+        public TransactionEvent TransactionEvent { get; set; }
+
+        public Guid PaymentId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        // Navigation properties
+        public virtual Payment Payment { get; set; } = null!;
+    }
+}
