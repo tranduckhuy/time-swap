fd88befd3851f3cea2bac1013c508bd31e1eaf53
huy01040104 <tronghuy01040@gmail.com>
2024-12-27 19:23:37 +0700

    add: share-banner component + assignee-detail component

--- frontend/src/app/modules/user/user.routes.ts
+++ frontend/src/app/modules/user/user.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AssigneesComponent } from './pages/assignee/assignees/assignees.component';
+import { AssigneeDetailComponent } from './pages/assignee/assignee-detail/assignee-detail.component';
 
 export const userRoutes: Routes = [
     {
@@ -15,5 +16,9 @@ export const userRoutes: Routes = [
     {
         path: 'assignees',
         component: AssigneesComponent
+    },
+    {
+        path: 'assignees/:assigneeId',
+        component: AssigneeDetailComponent
     }
 ]
