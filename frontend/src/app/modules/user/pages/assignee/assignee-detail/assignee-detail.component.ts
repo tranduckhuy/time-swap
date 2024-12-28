9464c4137596e541d63897a5aac210c9ab9ac24d
huy01040104 <tronghuy01040@gmail.com>
2024-12-28 22:40:00 +0700

    add: assignee detail component + profile + contact

--- frontend/src/app/modules/user/pages/assignee/assignee-detail/assignee-detail.component.ts
+++ frontend/src/app/modules/user/pages/assignee/assignee-detail/assignee-detail.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "../../../../../shared/components/banner/banner.component";
 import { BannerDetailComponent } from "../../../../../shared/components/banner/banner-detail/banner-detail.component";
+import { GridLayoutComponent } from "../../../../../core/layout/grid-layout/grid-layout.component";
+import { ButtonWithIconComponent } from "../../../../../shared/components/button-with-icon/button-with-icon.component";
 
 @Component({
   selector: 'app-assignee-detail',
   standalone: true,
-  imports: [BannerComponent, BannerDetailComponent],
+  imports: [BannerComponent, BannerDetailComponent, GridLayoutComponent, ButtonWithIconComponent],
   templateUrl: './assignee-detail.component.html',
   styleUrl: './assignee-detail.component.css'
 })
