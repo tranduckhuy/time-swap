d23c92a0051811cc1959cb44e73f11d855017a4a
huy01040104 <tronghuy01040@gmail.com>
2024-12-28 19:23:09 +0700

    add: posted-job component for profile + banner component

--- frontend/src/app/modules/user/pages/jobs/job-detail/job-detail.component.ts
+++ frontend/src/app/modules/user/pages/jobs/job-detail/job-detail.component.ts
@@ -6,11 +6,13 @@ import { JobPostComponent } from "../../../../../shared/components/job-post/job-
 import { GridLayoutComponent } from "../../../../../core/layout/grid-layout/grid-layout.component";
 import { JobAlertComponent } from "../../../../../shared/components/job-alert/job-alert.component";
 import { ButtonWithIconComponent } from "../../../../../shared/components/button-with-icon/button-with-icon.component";
+import { BannerDetailComponent } from "../../../../../shared/components/banner/banner-detail/banner-detail.component";
+import { BannerComponent } from "../../../../../shared/components/banner/banner.component";
 
 @Component({
   selector: 'app-job-detail',
   standalone: true,
-  imports: [JobPostComponent, GridLayoutComponent, JobAlertComponent, ButtonWithIconComponent],
+  imports: [JobPostComponent, GridLayoutComponent, JobAlertComponent, ButtonWithIconComponent, BannerDetailComponent, BannerComponent],
   templateUrl: './job-detail.component.html',
   styleUrl: './job-detail.component.css'
 })
