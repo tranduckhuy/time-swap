2b69803b52d77a1899ef2255bb8740e2e4e43bc8
huy01040104 <tronghuy01040@gmail.com>
2024-12-28 13:51:33 +0700

    add: UI profile component

---
+++ frontend/src/app/modules/user/pages/profile/profile-content/applied-jobs/applied-jobs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applied-jobs',
+  standalone: true,
+  imports: [],
+  templateUrl: './applied-jobs.component.html',
+  styleUrl: './applied-jobs.component.css'
+})
+export class AppliedJobsComponent {
+
+}
