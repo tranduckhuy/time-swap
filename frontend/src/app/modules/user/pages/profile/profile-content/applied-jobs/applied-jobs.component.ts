53a7b09b13422729585fc368daf04b0fc2d1d9ce
huy01040104 <tronghuy01040@gmail.com>
2024-12-28 19:23:09 +0700

    add: posted-job component for profile + banner component

--- frontend/src/app/modules/user/pages/profile/profile-content/applied-jobs/applied-jobs.component.ts
+++ frontend/src/app/modules/user/pages/profile/profile-content/applied-jobs/applied-jobs.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { DUMMY_JOBS } from '../../../jobs/job-list/dummy-job';
+import { JobPostComponent } from "../../../../../../shared/components/job-post/job-post.component";
 
 @Component({
   selector: 'app-applied-jobs',
   standalone: true,
-  imports: [],
+  imports: [JobPostComponent],
   templateUrl: './applied-jobs.component.html',
   styleUrl: './applied-jobs.component.css'
 })
 export class AppliedJobsComponent {
-
+  appliedJobs = DUMMY_JOBS.slice(0, 3);
 }
