cf474345900a6015e30e767aec6742b7ac8e999c
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-12-29 18:28:28 +0700

    feat: implement authentication APIs for user management, token handling

---
+++ backend/TimeSwap.Infrastructure/Email/MailMessageHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using TimeSwap.Application.Dtos.Email;
+using TimeSwap.Infrastructure.Identity;
+
+namespace TimeSwap.Infrastructure.Email
+{
+    public class MailMessageHelper
+    {
+        public static Message CreateMessage(ApplicationUser user, string token, string clientUri, string subject, string content)
+        {
+
+            var param = new Dictionary<string, string>
+            {
+                { "token", token },
+                { "email", user.Email! }
+            };
+
+            var callbackUrl = QueryHelpers.AddQueryString(clientUri, param!);
+
+            var message = new Message(
+                [(user.UserName!, user.Email!)],
+                subject,
+                //$"Please {content} by <a href='{callbackUrl}'>clicking here</a>.",
+                token,
+                null
+            );
+
+            return message;
+        }
+    }
+}
