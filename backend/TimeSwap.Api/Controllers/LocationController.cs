258b3ed7086701c883f0c22714c4a37aa356de03
Trần Đức Huy <duchuy81.1610@gmail.com>
2024-12-31 18:31:44 +0700

    feat: implement location api

---
+++ backend/TimeSwap.Api/Controllers/LocationController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TimeSwap.Application.Location.Queries;
+using TimeSwap.Application.Location.Responses;
+
+namespace TimeSwap.Api.Controllers
+{
+    public class LocationController : BaseController
+    {
+        public LocationController(IMediator mediator) : base(mediator) { }
+
+        [HttpGet("cities")]
+        public async Task<IActionResult> GetCities()
+        {
+            return await ExecuteAsync<GetCitiesQuery, IEnumerable<CityResponse>>(new GetCitiesQuery());
+        }
+
+        [HttpGet("cities/{cityId}/wards")]
+        public async Task<IActionResult> GetWardsByCityId(string cityId)
+        {
+            return await ExecuteAsync<GetWardsByCityIdQuery, IEnumerable<WardResponse>>(new GetWardsByCityIdQuery(cityId));
+        }
+    }
+}
