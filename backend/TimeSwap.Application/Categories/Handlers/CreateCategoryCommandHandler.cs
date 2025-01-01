bf182a0d65cb40f6e41768a714766b2cdc1e301b
SangTN5 <tranngocsang147877@gmail.com>
2025-01-01 14:50:07 +0700

    add: create industry API and category API

---
+++ backend/TimeSwap.Application/Categories/Handlers/CreateCategoryCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using TimeSwap.Application.Categories.Commands;
+using TimeSwap.Domain.Entities;
+using TimeSwap.Domain.Exceptions;
+using TimeSwap.Domain.Interfaces.Repositories;
+using TimeSwap.Shared.Constants;
+
+namespace TimeSwap.Application.Categories.Handlers
+{
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, int>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IIndustryRepository _industryRepository;
+
+        public CreateCategoryCommandHandler(ICategoryRepository categoryRepository, IIndustryRepository industryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _industryRepository = industryRepository;
+        }
+
+        public async Task<int> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var industry = await _industryRepository.GetByIdAsync(request.IndustryId);
+            if (industry == null)
+            {
+                throw new AppException(StatusCode.IndustryNotFound);
+            }
+
+            var category = new Category
+            {
+                CategoryName = request.CategoryName,
+                IndustryId = request.IndustryId
+            };
+
+            category = await _categoryRepository.AddAsync(category);
+            return category.Id;
+        }
+    }
+}
