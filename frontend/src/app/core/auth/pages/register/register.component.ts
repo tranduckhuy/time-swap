4ff1190acf04a150de367139eae5f741f6390367
Nguyen Quy <quynx9503.dev@gmail.com>
2025-01-02 17:30:53 +0700

    fix:Fix bug register do not have toast and Notify message not clearly enough
    - Before: Register do not have toast to display error message, even success message. Some notify in login, register pages not clearly enough to understand situation we are facing
    - After: Add toast for register page. Add more condition checking for display notify message more clearly for user

--- frontend/src/app/core/auth/pages/register/register.component.ts
+++ frontend/src/app/core/auth/pages/register/register.component.ts
@@ -4,23 +4,25 @@ import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { PreLoaderComponent } from '../../../../shared/components/pre-loader/pre-loader.component';
+import { ToastComponent } from "../../../../shared/components/toast/toast.component";
 
 import { controlValueEqual, getErrorMessage } from '../../../../shared/utils/form-validators';
 
 import { AUTH_CLIENT_URL } from '../../../../shared/constants/auth-constants';
-import { REGISTER_SUCCESS_CODE } from '../../../../shared/constants/status-code-constants';
-import { ERROR_MESSAGE, SUCCESS_MESSAGE } from '../../../../shared/constants/message-constants';
+import { EMAIL_EXIST_CODE, REGISTER_CONFIRM_SUCCESS_CODE } from '../../../../shared/constants/status-code-constants';
 
 import type { RegisterRequestModel } from '../../../../shared/models/api/request/register-request.model';
 
 import { AuthService } from '../../auth.service';
 import { ToastService } from '../../../../shared/services/toast.service';
 import { MultiLanguageService } from '../../../../shared/services/multi-language.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { showToast } from '../../../../shared/utils/util-functions';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, TranslateModule, RouterLink, PreLoaderComponent],
+  imports: [ReactiveFormsModule, TranslateModule, RouterLink, PreLoaderComponent, ToastComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -74,11 +76,43 @@ export class RegisterComponent implements OnInit {
     };
     const subscription = this.authService.register(req).subscribe({
       next: (res) => {
-        if (res.statusCode === REGISTER_SUCCESS_CODE) {
-          this.toastService.success(SUCCESS_MESSAGE, this.multiLanguageService.getTranslatedLang('auth.register.success'));
+        if (res.statusCode === REGISTER_CONFIRM_SUCCESS_CODE) {
+          showToast(
+            this.toastService, 
+            this.multiLanguageService, 
+            'success', 
+            'common.notify.success-title', 
+            'auth.register.success'
+          );
+        } else {
+          showToast(
+            this.toastService, 
+            this.multiLanguageService, 
+            'error', 
+            'common.notify.error-title', 
+            'auth.register.failure'
+          );
         }
       },
-      error: () => this.toastService.error(ERROR_MESSAGE, this.multiLanguageService.getTranslatedLang('common.messages.error'))
+      error: (error: HttpErrorResponse) => {
+        if (error.error.statusCode === EMAIL_EXIST_CODE) {
+          showToast(
+            this.toastService, 
+            this.multiLanguageService, 
+            'warn', 
+            'common.notify.warning-title', 
+            'auth.register.email-exist'
+          );
+        } else {
+          showToast(
+            this.toastService, 
+            this.multiLanguageService, 
+            'error', 
+            'common.notify.error-title', 
+            'common.notify.error-message'
+          );
+        }
+      }
     });
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
--- frontend/src/app/shared/constants/message-constants.ts
+++
@@ -1,2 +0,0 @@
-export const SUCCESS_MESSAGE = 'Success';
-export const ERROR_MESSAGE = 'Error';
