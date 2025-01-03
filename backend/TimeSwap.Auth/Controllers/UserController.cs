68c15078e676f23c70c33b3debce15ca89ad9f12
Trần Đức Huy <duchuy81.1610@gmail.com>
2025-01-03 18:18:08 +0700

    feat: implement get user profile api

---
+++ backend/TimeSwap.Auth/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TimeSwap.Application.Authentication;
+using TimeSwap.Application.Authentication.Interfaces;
+using TimeSwap.Shared;
+using TimeSwap.Shared.Constants;
+
+namespace TimeSwap.Auth.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : BaseController<UserController>
+    {
+        private readonly IUserService _userService;
+
+        public UserController(ILogger<UserController> logger, IUserService userService) : base(logger)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("profile")]
+        [Authorize]
+        public async Task<IActionResult> GetUserProfileAsync()
+        {
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId == null)
+            {
+                return Unauthorized(new ApiResponse<object>
+                {
+                    StatusCode = (int)Shared.Constants.StatusCode.InvalidToken,
+                    Message = ResponseMessages.GetMessage(Shared.Constants.StatusCode.InvalidToken),
+                    Errors = ["User id does not exist in the claims"]
+                });
+            }
+
+            return await HandleRequestWithResponseAsync<Guid, Guid, UserResponse>(Guid.Parse(userId), _userService.GetUserProfileAsync);
+        }
+    }
+}
