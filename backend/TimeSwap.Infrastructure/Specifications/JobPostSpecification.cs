a643aad658bc119d70c7a74b411e7d54b21f42b8
Trần Đức Huy <duchuy81.1610@gmail.com>
2025-01-03 22:21:43 +0700

    update: update posted date in job post specification param

--- backend/TimeSwap.Infrastructure/Specifications/JobPostSpecification.cs
+++ backend/TimeSwap.Infrastructure/Specifications/JobPostSpecification.cs
@@ -3,6 +3,8 @@ using System.Linq.Expressions;
 using TimeSwap.Domain.Entities;
 using TimeSwap.Domain.Specs;
 using TimeSwap.Domain.Specs.Job;
+using TimeSwap.Shared.Constants;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TimeSwap.Infrastructure.Specifications
 {
@@ -17,17 +19,32 @@ namespace TimeSwap.Infrastructure.Specifications
 
         public JobPostSpecification(JobPostSpecParam param)
         {
+            DateTime? postedDate = null;
+
+            if (param.PostedDate != null)
+            {
+
+                postedDate = param.PostedDate switch
+                {
+                    PostedDate.Today => DateTime.UtcNow.Date,
+                    PostedDate.Yesterday => DateTime.UtcNow.Date.AddDays(-1),
+                    PostedDate.Last7Days => DateTime.UtcNow.Date.AddDays(-7),
+                    PostedDate.Last30Days => DateTime.UtcNow.Date.AddDays(-30),
+                    _ => null,
+                };
+            }
+
             // Build Criteria (e.g., Search, Filters)
             Criteria = x =>
-                (string.IsNullOrEmpty(param.Search) || EF.Functions.Like(EF.Functions.Unaccent(x.Title).ToLower(), $"%{param.Search.ToLower()}%")) &&
-                (param.IndustryId == 0 || x.IndustryId == param.IndustryId) &&
-                (param.CategoryId == 0 || x.CategoryId == param.CategoryId) &&
-                (param.MinFee == null || x.Fee >= param.MinFee) &&
-                (param.MaxFee == null || x.Fee <= param.MaxFee) &&
-                (param.PostedDate == null || x.CreatedAt >= param.PostedDate) &&
-                (string.IsNullOrEmpty(param.CityId) || x.CityId == param.CityId) &&
-                (string.IsNullOrEmpty(param.WardId) || x.WardId == param.WardId) &&
-                (x.IsActive == param.IsActive);
+            (string.IsNullOrEmpty(param.Search) || EF.Functions.Like(EF.Functions.Unaccent(x.Title).ToLower(), $"%{param.Search.ToLower()}%")) &&
+            (param.IndustryId == 0 || x.IndustryId == param.IndustryId) &&
+            (param.CategoryId == 0 || x.CategoryId == param.CategoryId) &&
+            (param.MinFee == null || x.Fee >= param.MinFee) &&
+            (param.MaxFee == null || x.Fee <= param.MaxFee) &&
+            (param.PostedDate == null || x.CreatedAt >= postedDate) &&
+            (string.IsNullOrEmpty(param.CityId) || x.CityId == param.CityId) &&
+            (string.IsNullOrEmpty(param.WardId) || x.WardId == param.WardId) &&
+            (x.IsActive == param.IsActive);
 
             // Sorting logic
             if (!string.IsNullOrEmpty(param.Sort))
