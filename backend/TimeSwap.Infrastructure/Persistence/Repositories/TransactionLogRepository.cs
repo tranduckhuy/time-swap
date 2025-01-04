f9b48c56e76b824cfdf5df3e872fe19f129f857a
SangTN5 <tranngocsang147877@gmail.com>
2025-01-04 08:25:25 +0700

    add: create API payment VNPAY and update model mapping

---
+++ backend/TimeSwap.Infrastructure/Persistence/Repositories/TransactionLogRepository.cs
@@ -0,0 +1,12 @@
+﻿using TimeSwap.Domain.Entities;
+using TimeSwap.Domain.Interfaces.Repositories;
+using TimeSwap.Infrastructure.Persistence.DbContexts;
+
+namespace TimeSwap.Infrastructure.Persistence.Repositories
+{
+    public class TransactionLogRepository : RepositoryBase<TransactionLog, Guid>, ITransactionLogRepository
+    {
+        public TransactionLogRepository(AppDbContext context) : base(context) { }
+
+    }
+}
