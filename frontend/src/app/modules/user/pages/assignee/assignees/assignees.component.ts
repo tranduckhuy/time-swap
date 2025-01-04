2591681ef9ac4edeb15ed29957703ebee800d30b
huy01040104 <tronghuy01040@gmail.com>
2025-01-04 10:09:12 +0700

    feat: header profile + switch language

--- frontend/src/app/modules/user/pages/assignee/assignees/assignees.component.ts
+++ frontend/src/app/modules/user/pages/assignee/assignees/assignees.component.ts
@@ -3,11 +3,12 @@ import { BannerComponent } from "../../../../../shared/components/banner/banner.
 import { BreadcrumbComponent } from "../../../../../shared/components/breadcrumb/breadcrumb.component";
 import { AssigneeCardComponent } from "./assignee-card/assignee-card.component";
 import { PaginationComponent } from '../../../../../shared/components/pagination/pagination.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-assignees',
   standalone: true,
-  imports: [BannerComponent, BreadcrumbComponent, AssigneeCardComponent, PaginationComponent],
+  imports: [BannerComponent, BreadcrumbComponent, AssigneeCardComponent, PaginationComponent, TranslateModule],
   templateUrl: './assignees.component.html',
   styleUrl: './assignees.component.css'
 })
