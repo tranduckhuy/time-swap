946e7cb42c442d46e2828fe36573ec08552c3d50
SangTN5 <tranngocsang147877@gmail.com>
2025-01-04 17:39:20 +0700

    add: get payment by user id for API payment

---
+++ backend/TimeSwap.Application/Payments/Responses/PaymentDetailResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace TimeSwap.Application.Payments.Responses
+{
+    public class PaymentDetailResponse
+    {
+        public Guid PaymentId { get; set; }
+
+        public string PaymentContent { get; set; } = string.Empty;
+
+        public decimal Amount { get; set; }
+
+        public string PaymentStatus { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public string PaymentMethodType { get; set; } = string.Empty;
+
+        public string MethodDetailName { get; set; } = string.Empty;
+    }
+}
--- backend/TimeSwap.Domain/Interfaces/Repositories/IPaymentMethodRepository.cs
+++ backend/TimeSwap.Domain/Interfaces/Repositories/IPaymentMethodRepository.cs
@@ -4,5 +4,6 @@ namespace TimeSwap.Domain.Interfaces.Repositories
 {
     public interface IPaymentMethodRepository : IAsyncRepository<PaymentMethod, int>
     {
+
     }
 }
