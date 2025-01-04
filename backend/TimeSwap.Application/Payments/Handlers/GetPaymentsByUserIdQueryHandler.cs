bc73eae65d2dea67c34a269d4acf8cfd847cf33c
SangTN5 <tranngocsang147877@gmail.com>
2025-01-04 17:39:20 +0700

    add: get payment by user id for API payment

---
+++ backend/TimeSwap.Application/Payments/Handlers/GetPaymentsByUserIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using TimeSwap.Application.Exceptions.Payments;
+using TimeSwap.Application.Mappings;
+using TimeSwap.Application.Payments.Queries;
+using TimeSwap.Application.Payments.Responses;
+using TimeSwap.Domain.Interfaces.Repositories;
+
+namespace TimeSwap.Application.Payments.Handlers
+{
+    public class GetPaymentsByUserIdQueryHandler : IRequestHandler<GetPaymentsByUserIdQuery, List<PaymentDetailResponse>>
+    {
+        private readonly IPaymentRepository _paymentRepository;
+
+        public GetPaymentsByUserIdQueryHandler(IPaymentRepository paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
+
+        public async Task<List<PaymentDetailResponse>> Handle(GetPaymentsByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var payments = await _paymentRepository.GetPaymentsByUserIdAsync(request.UserId);
+
+            if (!payments.Any())
+            {
+                throw new PaymentNotFoundByUserIdException();
+            }
+
+            return AppMapper<CoreMappingProfile>.Mapper.Map<List<PaymentDetailResponse>>(payments);
+        }
+    }
+}
