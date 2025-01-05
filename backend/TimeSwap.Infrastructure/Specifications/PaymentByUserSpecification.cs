8d45eae8d33ef11d7fbcfa122e68674e3399b3f6
SangTN5 <tranngocsang147877@gmail.com>
2025-01-05 15:52:47 +0700

    fix: fix sonar scan

--- backend/TimeSwap.Infrastructure/Specifications/PaymentByUserSpecification.cs
+++ backend/TimeSwap.Infrastructure/Specifications/PaymentByUserSpecification.cs
@@ -8,11 +8,17 @@ namespace TimeSwap.Infrastructure.Specifications
     public class PaymentByUserSpecification : ISpecification<Payment>
     {
         public Expression<Func<Payment, bool>> Criteria { get; private set; }
+
         public Func<IQueryable<Payment>, IOrderedQueryable<Payment>>? OrderBy { get; private set; }
+
         public Func<IQueryable<Payment>, IOrderedQueryable<Payment>>? OrderByDescending { get; private set; }
+
         public List<Expression<Func<Payment, object>>> Includes { get; private set; } = [];
+
         public int Skip { get; private set; }
+
         public int Take { get; private set; }
+
 
         public PaymentByUserSpecification(Guid userId, string? paymentStatus, int dateFilter, int pageIndex = 1, int pageSize = 10)
         {
@@ -23,16 +29,35 @@ namespace TimeSwap.Infrastructure.Specifications
                 Enum.TryParse(paymentStatus, true, out parsedStatus);
             }
 
+            DateTime? startDate = null;
+            DateTime? endDate = null;
+
+            switch (dateFilter)
+            {
+                case 0:
+                    startDate = DateTime.UtcNow.Date;
+                    endDate = DateTime.UtcNow.Date.AddDays(1);
+                    break;
+                case 1:
+                    startDate = DateTime.UtcNow.Date.AddDays(-1);
+                    endDate = DateTime.UtcNow.Date;
+                    break;
+                case 2:
+                    startDate = DateTime.UtcNow.AddDays(-7);
+                    endDate = DateTime.UtcNow;
+                    break;
+                case 3:
+                    startDate = DateTime.UtcNow.AddDays(-30);
+                    endDate = DateTime.UtcNow;
+                    break;
+            }
+
             Criteria = payment =>
                 payment.UserId == userId &&
                 (string.IsNullOrEmpty(paymentStatus) || payment.PaymentStatus == parsedStatus) &&
-                (dateFilter == 0 ? payment.CreatedAt >= DateTime.UtcNow.Date && payment.CreatedAt < DateTime.UtcNow.Date.AddDays(1) :
-                 dateFilter == 1 ? payment.CreatedAt >= DateTime.UtcNow.Date.AddDays(-1) && payment.CreatedAt < DateTime.UtcNow.Date :
-                 dateFilter == 2 ? payment.CreatedAt >= DateTime.UtcNow.AddDays(-7) && payment.CreatedAt < DateTime.UtcNow :
-                 dateFilter != 3 || payment.CreatedAt >= DateTime.UtcNow.AddDays(-30) && payment.CreatedAt < DateTime.UtcNow);
+                (startDate == null || (payment.CreatedAt >= startDate && payment.CreatedAt < endDate));
 
             OrderBy = q => q.OrderBy(p => p.CreatedAt);
-
             Skip = (pageIndex - 1) * pageSize;
             Take = pageSize;
 
