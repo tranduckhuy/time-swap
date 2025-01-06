113f808306314191bced35a3164878c2e0ea8807
SangTN5 <tranngocsang147877@gmail.com>
2025-01-06 10:21:55 +0700

    fix: remove paging for get category by industry id

--- backend/TimeSwap.Application/Industries/Queries/GetCategoriesByIndustryQuery.cs
+++ backend/TimeSwap.Application/Industries/Queries/GetCategoriesByIndustryQuery.cs
@@ -1,15 +1,10 @@
 ﻿using MediatR;
 using TimeSwap.Application.Categories.Responses;
-using TimeSwap.Domain.Specs;
 
 namespace TimeSwap.Application.Industries.Queries
 {
-    public class GetCategoriesByIndustryQuery : IRequest<Pagination<CategoryResponse>>
+    public class GetCategoriesByIndustryQuery : IRequest<List<CategoryResponse>>
     {
         public int IndustryId { get; set; }
-
-        public int PageIndex { get; set; } = 1;
-
-        public int PageSize { get; set; } = 10;
     }
 }
