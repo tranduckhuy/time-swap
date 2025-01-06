8a456f802cb2d4b520636c29fd01e569ac659e08
Trần Đức Huy <duchuy81.1610@gmail.com>
2025-01-06 16:39:41 +0700

    feat: implement get job post by user Id

--- backend/TimeSwap.Application/JobPosts/Handlers/GetJobPostByIdQueryHandler.cs
+++ backend/TimeSwap.Application/JobPosts/Handlers/GetJobPostByIdQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using TimeSwap.Application.Exceptions.JobPost;
 using TimeSwap.Application.JobPosts.Queries;
 using TimeSwap.Application.JobPosts.Responses;
 using TimeSwap.Application.Mappings;
@@ -6,21 +7,31 @@ using TimeSwap.Domain.Interfaces.Repositories;
 
 namespace TimeSwap.Application.JobPosts.Handlers
 {
-    public class GetJobPostByIdQueryHandler : IRequestHandler<GetJobPostByIdQuery, JobPostResponse>
+    public class GetJobPostByIdQueryHandler : IRequestHandler<GetJobPostByIdQuery, JobPostDetailResponse>
     {
         private readonly IJobPostRepository _jobPostRepository;
+        private readonly IJobApplicantRepository _jobApplicantRepository;
 
-        public GetJobPostByIdQueryHandler(IJobPostRepository jobPostRepository)
+        public GetJobPostByIdQueryHandler(IJobPostRepository jobPostRepository, IJobApplicantRepository jobApplicantRepository)
         {
             _jobPostRepository = jobPostRepository;
+            _jobApplicantRepository = jobApplicantRepository;
         }
 
-        public async Task<JobPostResponse> Handle(GetJobPostByIdQuery request, CancellationToken cancellationToken)
+        public async Task<JobPostDetailResponse> Handle(GetJobPostByIdQuery request, CancellationToken cancellationToken)
         {
-            var jobPost = await _jobPostRepository.GetJobPostByIdAsync(request.Id);
+            var jobPost = await _jobPostRepository.GetJobPostByIdAsync(request.Id) ?? throw new JobPostNotFoundException();
 
-            var jobPostResponse = AppMapper<CoreMappingProfile>.Mapper.Map<JobPostResponse>(jobPost);
-            
+            var toalApplicants = await _jobApplicantRepository.GetTotalApplicantsByJobPostIdAsync(request.Id);
+
+            var jobPostResponse = AppMapper<CoreMappingProfile>.Mapper.Map<JobPostDetailResponse>(jobPost);
+
+            jobPostResponse.TotalApplicants = toalApplicants;
+
+            var relatedJobPosts = await _jobPostRepository.GetRelatedJobPostsAsync(jobPost.Id, jobPost.Category.Id, jobPost.Industry.Id, 3);
+
+            jobPostResponse.RelatedJobPosts = AppMapper<CoreMappingProfile>.Mapper.Map<IEnumerable<JobPostResponse>>(relatedJobPosts);
+
             return jobPostResponse;
         }
     }
