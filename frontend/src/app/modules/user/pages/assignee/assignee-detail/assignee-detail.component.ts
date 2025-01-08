0bdf14047be25309486472229241ebf6bb04fdf8
Nguyen Quy <quynx9503.dev@gmail.com>
2025-01-08 09:57:42 +0700

    feat:Add review-card, reivew-form components and Add about component
    - Before: Do not have any component related to review
    - After: Add review-card and review-form components

--- frontend/src/app/modules/user/pages/assignee/assignee-detail/assignee-detail.component.ts
+++ frontend/src/app/modules/user/pages/assignee/assignee-detail/assignee-detail.component.ts
@@ -1,14 +1,26 @@
-import { TranslateModule } from '@ngx-translate/core';
 import { Component } from '@angular/core';
+
+import { TranslateModule } from '@ngx-translate/core';
+
 import { BannerComponent } from "../../../../../shared/components/banner/banner.component";
 import { BannerDetailComponent } from "../../../../../shared/components/banner/banner-detail/banner-detail.component";
 import { GridLayoutComponent } from "../../../../../core/layout/grid-layout/grid-layout.component";
 import { ButtonWithIconComponent } from "../../../../../shared/components/button-with-icon/button-with-icon.component";
+import { ReviewCardComponent } from "../../../../../shared/components/review-card/review-card.component";
+import { ReviewFormComponent } from "../../../../../shared/components/review-form/review-form.component";
 
 @Component({
   selector: 'app-assignee-detail',
   standalone: true,
-  imports: [BannerComponent, BannerDetailComponent, GridLayoutComponent, ButtonWithIconComponent, TranslateModule],
+  imports: [
+    TranslateModule, 
+    BannerComponent, 
+    BannerDetailComponent, 
+    GridLayoutComponent, 
+    ButtonWithIconComponent, 
+    ReviewCardComponent, 
+    ReviewFormComponent
+  ],
   templateUrl: './assignee-detail.component.html',
   styleUrl: './assignee-detail.component.css'
 })
