25a00c8e1c34d72159235b77e640e0512309e92a
Nguyen Quy <quynx9503.dev@gmail.com>
2025-01-11 20:32:55 +0700

    refactor:Refactor thousand pipe
    - Before: Using regex for display thousand value
    - After: Using Intl for format value by locale, optimize with i18n

--- frontend/src/app/shared/pipes/thousand.pipe.ts
+++ frontend/src/app/shared/pipes/thousand.pipe.ts
@@ -1,13 +1,35 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { formatNumberValue } from '../utils/util-functions';
 
 @Pipe({
   name: 'thousand',
   standalone: true
 })
 export class ThousandPipe implements PipeTransform {
+  private readonly DEFAULT_LANGUAGE = 'vi' as const;
+  private readonly LOCALES = {
+    vi: 'vi-VN',
+    en: 'en-US'
+  } as const
 
-  transform(value: number | string): string {
-    if (!value) return '';
-    return value.toString().replace(/(?<=\d)(?=(?:\d{3})+$)/g, ',');
+  transform(
+    value: number | string | null | undefined, 
+    lang: 'vi' | 'en' = this.DEFAULT_LANGUAGE
+  ): string {
+    if (!value) 
+      return '';
+    
+    const numericValue = this.parseValue(value);
+    if (isNaN(numericValue))
+      return '';
+
+    const locale = this.LOCALES[lang];
+
+    return formatNumberValue(numericValue, locale);
+  }
+
+  private parseValue(value: number | string): number {
+    const parsedValue = typeof value === 'string' ? parseFloat(value) : value;
+    return isNaN(parsedValue) ? NaN : parsedValue; 
   }
 }
